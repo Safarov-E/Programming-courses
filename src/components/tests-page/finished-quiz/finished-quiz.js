09da8b907c636709296b741977fac06a12599e37
safarov-e <elkhan.safarov10@yandex.com>
2021-02-26 14:34:10 +0500

    Вычисление результатов

--- src/components/tests-page/finished-quiz/finished-quiz.js
+++ src/components/tests-page/finished-quiz/finished-quiz.js
@@ -3,19 +3,31 @@ import './finished-quiz.css'
 
 export default class FinishedQuiz extends Component {
     render() {
+        const {quiz, results, onRetry} = this.props;
+        const successCount = Object.keys(results).reduce((total, key) => {
+            if(results[key] === 'check-success') {
+                total++
+            }
+            return total
+        }, 0)
         return (
-            <div className="content__block-test">
-                <div className="content__test-list">
-                    <div className="finished-quiz">
-                        <ul>
-                            <li>1. Тест <i className="fa fa-times times-error" /></li>
-                            <li>2. Тест <i className="fa fa-check check-success" /></li>
-                        </ul>
-                        <p>Правильно 4 из 10</p>
-                        <div className="return-quiz">
-                            <a href="#">Повторить</a>
-                        </div>
-                    </div>
+            <div className="finished-quiz">
+                <ul>
+                    {
+                        quiz.map((quizItem, index) => {
+                            return (
+                                <li key={index}>{index + 1}. {quizItem.question} 
+                                    <i className={results[quizItem.id] === 'times-error' ? 
+                                                    'fa fa-times ' + results[quizItem.id] : 
+                                                    'fa fa-check ' + results[quizItem.id]} />
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+                <p>Правильно {successCount} из {quiz.length}</p>
+                <div className="return-quiz">
+                    <button onClick={onRetry}>Повторить</button>
                 </div>
             </div>
         )
--- src/components/tests-page/list-questions/list-questions.js
+++ src/components/tests-page/list-questions/list-questions.js
@@ -8,6 +8,7 @@ import './list-questions.css'
 
 export default class ListQuestions extends Component {
     state = {
+        results: {},
         isFinished: false,
         activeQuestion: 0,
         answerState: null,
@@ -18,9 +19,14 @@ export default class ListQuestions extends Component {
             if(this.state.answerState[key] === 'success') return
         }
         const question = data[this.state.activeQuestion]
+        const results = this.state.results
         if(question.rightAnswerId === answeId) {
+            if(!results[question.id]) {
+                results[question.id] = 'check-success'
+            }
             this.setState({
-                answerState: {[answeId]: 'success'}
+                answerState: {[answeId]: 'success'},
+                results
             })
             const timeout = window.setTimeout(() => {
                 if(this.isQuizFinished()) {
@@ -37,24 +43,39 @@ export default class ListQuestions extends Component {
                 window.clearTimeout(timeout)
             }, 1000)
         } else {
+            results[question.id] = 'times-error'
             this.setState({
-                answerState: {[answeId]: 'error'}
+                answerState: {[answeId]: 'error'},
+                results
             })
         }
     }
     isQuizFinished() {
         return this.state.activeQuestion + 1 === data.length
     }
+    retryHandler = () => {
+        this.setState({
+            activeQuestion: 0,
+            answerState: null,
+            isFinished: false,
+            results: {}
+        })
+    }
     render() {
-        const {activeQuestion, answerState, isFinished} = this.state
+        const {activeQuestion, answerState, isFinished, results} = this.state
+        console.log(results)
         return (
             <>
                 <HomePage />
                 <div className="content__block-test">
                     <div className="content__test-list">
                         {
-                            !isFinished
-                            ? <FinishedQuiz />
+                            isFinished
+                            ?   <FinishedQuiz 
+                                    results={results}
+                                    quiz={data}
+                                    onRetry={this.retryHandler}
+                                />
                             : <>
                                 <div className="content__test-title">
                                     <h2>Тест по основам HTML</h2>
