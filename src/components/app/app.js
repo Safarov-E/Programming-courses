be4bdb5c2545c5b4549a7a3db10791f980a4fcee
safarov-e <elkhan.safarov10@yandex.com>
2021-03-26 15:57:08 +0500

    Code Splitting with React

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,53 +1,56 @@
-import React, { Component } from 'react'
-import {Route, Switch} from 'react-router-dom'
-import CategoryBlock from '../category-block'
-import BookPage from '../book-page/book-page'
-import TestsPage from '../tests-page/tests-page'
-import ProductPage from '../book-page/product-page'
-import HtmlTest from '../tests-page/list-tests/html-test'
-import PHPTest from '../tests-page/list-tests/php-test'
-import CSSTest from '../tests-page/list-tests/css-test'
-import JSTest from '../tests-page/list-tests/javascript-test'
-import SQLTest from '../tests-page/list-tests/sql-test'
-import PageNotFound from '../page-not-found'
-import Courses from '../courses'
-import ListOfCourses from '../courses/list-of-courses'
-import JavaScriptLessons from '../courses/video-lessons/lessons/javascript-lessons'
-import PHPLessons from '../courses/video-lessons/lessons/php-lessons'
-import WordPressLessons from '../courses/video-lessons/lessons/wordpress-lessons'
-import TypeScriptLessons from '../courses/video-lessons/lessons/typescript-lessons'
-import NodeLessons from '../courses/video-lessons/lessons/node-lessons'
-import HtmlLessons from '../courses/video-lessons/lessons/html-lessons'
-import CsslLessons from '../courses/video-lessons/lessons/css-lessons'
-import PythonLessons from '../courses/video-lessons/lessons/python-lessons'
-import JavaLessons from '../courses/video-lessons/lessons/java-lessons'
-import VueLessons from '../courses/video-lessons/lessons/vue-lessons'
+import React, { lazy, Suspense, Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+
+const CategoryBlock = lazy(() => import('../category-block'));
+const BookPage = lazy(() => import('../book-page/book-page'));
+const TestsPage = lazy(() => import('../tests-page/tests-page'));
+const ProductPage = lazy(() => import('../book-page/product-page'));
+const HtmlTest = lazy(() => import('../tests-page/list-tests/html-test'));
+const PHPTest = lazy(() => import('../tests-page/list-tests/php-test'));
+const CSSTest = lazy(() => import('../tests-page/list-tests/css-test'));
+const JSTest = lazy(() => import('../tests-page/list-tests/javascript-test'));
+const SQLTest = lazy(() => import('../tests-page/list-tests/sql-test'));
+const PageNotFound = lazy(() => import('../page-not-found'));
+const Courses = lazy(() => import('../courses'));
+const ListOfCourses = lazy(() => import('../courses/list-of-courses'));
+const JavaScriptLessons = lazy(() => import('../courses/video-lessons/lessons/javascript-lessons'));
+const PHPLessons = lazy(() => import('../courses/video-lessons/lessons/php-lessons'));
+const WordPressLessons = lazy(() => import('../courses/video-lessons/lessons/wordpress-lessons'));
+const TypeScriptLessons = lazy(() => import('../courses/video-lessons/lessons/typescript-lessons'));
+const NodeLessons = lazy(() => import('../courses/video-lessons/lessons/node-lessons'));
+const HtmlLessons = lazy(() => import('../courses/video-lessons/lessons/html-lessons'));
+const CsslLessons = lazy(() => import('../courses/video-lessons/lessons/css-lessons'));
+const PythonLessons = lazy(() => import('../courses/video-lessons/lessons/python-lessons'));
+const JavaLessons = lazy(() => import('../courses/video-lessons/lessons/java-lessons'));
+const VueLessons = lazy(() => import('../courses/video-lessons/lessons/vue-lessons'));
 
 export default class App extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" exact component={CategoryBlock} />
-                <Route path="/books" exact component={BookPage} />
-                <Route path="/tests" exact component={TestsPage} />
-                <Route path="/tests/html-test" exact component={HtmlTest} />
-                <Route path="/tests/php-test" exact component={PHPTest} />
-                <Route path="/tests/css-test" exact component={CSSTest} />
-                <Route path="/tests/javascript-test" exact component={JSTest} />
-                <Route path="/tests/sql-test" exact component={SQLTest} />
-                <Route path="/books/:id" exact component={ProductPage} />
-                <Route path="/course" exact component={Courses} />
-                <Route path="/course/:id" exact component={ListOfCourses} />
-                <Route path="/course/1/:num" exact component={JavaScriptLessons} />
-                <Route path="/course/2/:num" exact component={PHPLessons} />
-                <Route path="/course/3/:num" exact component={WordPressLessons} />
-                <Route path="/course/4/:num" exact component={TypeScriptLessons} />
-                <Route path="/course/5/:num" exact component={NodeLessons} />
-                <Route path="/course/6/:num" exact component={HtmlLessons} />
-                <Route path="/course/7/:num" exact component={CsslLessons} />
-                <Route path="/course/8/:num" exact component={PythonLessons} />
-                <Route path="/course/9/:num" exact component={JavaLessons} />
-                <Route path="/course/10/:num" exact component={VueLessons} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Route path="/" exact component={CategoryBlock} />
+                    <Route path="/books" exact component={BookPage} />
+                    <Route path="/tests" exact component={TestsPage} />
+                    <Route path="/tests/html-test" exact component={HtmlTest} />
+                    <Route path="/tests/php-test" exact component={PHPTest} />
+                    <Route path="/tests/css-test" exact component={CSSTest} />
+                    <Route path="/tests/javascript-test" exact component={JSTest} />
+                    <Route path="/tests/sql-test" exact component={SQLTest} />
+                    <Route path="/books/:id" exact component={ProductPage} />
+                    <Route path="/course" exact component={Courses} />
+                    <Route path="/course/:id" exact component={ListOfCourses} />
+                    <Route path="/course/1/:num" exact component={JavaScriptLessons} />
+                    <Route path="/course/2/:num" exact component={PHPLessons} />
+                    <Route path="/course/3/:num" exact component={WordPressLessons} />
+                    <Route path="/course/4/:num" exact component={TypeScriptLessons} />
+                    <Route path="/course/5/:num" exact component={NodeLessons} />
+                    <Route path="/course/6/:num" exact component={HtmlLessons} />
+                    <Route path="/course/7/:num" exact component={CsslLessons} />
+                    <Route path="/course/8/:num" exact component={PythonLessons} />
+                    <Route path="/course/9/:num" exact component={JavaLessons} />
+                    <Route path="/course/10/:num" exact component={VueLessons} />
+                </Suspense>
                 <Route component={PageNotFound} />
             </Switch>
         )
